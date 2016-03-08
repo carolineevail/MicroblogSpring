8e1e030e35ed8f7edf49b37f617c5f5d367583c7
carolineevail <carolineevail@gmail.com>
2016-03-08 09:27:11 -0500

    Completed

--- src/main/java/com/theironyard/MicroblogSpringApplicationController.java
+++ src/main/java/com/theironyard/MicroblogSpringApplicationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 /**
  * Created by Caroline on 3/7/16.
@@ -13,18 +14,36 @@ import javax.servlet.http.HttpSession;
 @Controller
 public class MicroblogSpringApplicationController {
 
+    ArrayList<Message> messages = new ArrayList<Message>();
+
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(Model model, HttpSession session) {
         model.addAttribute("name", session.getAttribute("userName"));
+        model.addAttribute("messages", messages);
         return "home";
     }
 
-    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
     public String login(HttpSession session, String userName) {
         session.setAttribute("userName", userName);
         return "redirect:/";
     }
+
+    @RequestMapping(path = "/add-message", method = RequestMethod.POST)
+    public String addMessage(String text) {
+        Message message = new Message(messages.size() + 1, text);
+        messages.add(message);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/delete-message", method = RequestMethod.POST)
+    public String deleteMessage(int id) {
+        messages.remove(messages.size() - 1);
+        return "redirect:/";
+    }
+
 }
+
 
 
 
